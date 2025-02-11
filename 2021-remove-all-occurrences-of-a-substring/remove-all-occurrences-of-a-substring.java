f941e6e3e25d5b3f4d3535acca59769666137736
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2025-02-12 00:52:12 +0530

    Time: 4 ms (49.85%) | Memory: 44.8 MB (19.65%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,24 +1,13 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        int n = s.length(), m = part.length();
+        int m = part.length();
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
-            result.append(s.charAt(i)); // Add current character to result
+        for (char c : s.toCharArray()) {
+            result.append(c);
 
-            // Check if the last part of result matches 'part'
-            if (result.length() >= m) {
-                boolean isMatch = true;
-                for (int j = 0; j < m; j++) {
-                    if (result.charAt(result.length() - m + j) != part.charAt(j)) {
-                        isMatch = false;
-                        break;
-                    }
-                }
-                // If matched, remove the last 'm' characters
-                if (isMatch) {
-                    result.delete(result.length() - m, result.length());
-                }
+            if (result.length() >= m && result.substring(result.length() - m).equals(part)) {
+                result.setLength(result.length() - m); // Efficiently remove last 'm' characters
             }
         }
         return result.toString();
