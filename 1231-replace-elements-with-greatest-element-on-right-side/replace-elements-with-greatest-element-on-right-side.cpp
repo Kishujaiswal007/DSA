3710b8bc4de998cf21986aafe141dc999419f87e
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-01-04 00:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 72.8 MB (42.94%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        vector<int> result(arr.size(), -1);
-        if(arr.size()==0) return result;
-        for(int i=0;i<arr.size()-1;i++){
-            int maxArr = arr[i+1];
-            for(int j=i+2;j<arr.size();j++){
-                maxArr=max(maxArr,arr[j]);
-            }
-            result[i]= maxArr;
-        }
-        return result;
+        int maxCurr=-1;
+        for(int i=arr.size()-1;i>=0;i--){
+            int curElem=arr[i];
+            arr[i]=maxCurr;
+            if(curElem>maxCurr)
+            {maxCurr=curElem;}
+
+        }return arr;
     }
 };
