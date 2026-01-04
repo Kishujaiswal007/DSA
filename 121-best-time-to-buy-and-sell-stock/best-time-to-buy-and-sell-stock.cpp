5370b67c0f509f39fadff7f7b948e3f718fea854
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-01-04 13:03:14 +0530

    Time: 4 ms (18.64%) | Memory: 97.2 MB (99.02%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,9 @@ public:
         int maxProfit=0;
         for(int i=1;i<prices.size();i++){
             minPrice=min(prices[i],minPrice);
-             int temp=prices[i]-minPrice;
-            maxProfit=max(maxProfit,temp);
-             temp=0;
+            // int temp=prices[i]-minPrice;
+            maxProfit=max(maxProfit,prices[i]-minPrice);
+            // temp=0;
         }return maxProfit;
         
     }
