725c373475ef5d946ba526d905eff54a31b913b1
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-01-16 21:34:46 +0530

    Time: 75 ms (11.93%) | Memory: 14 MB (80.34%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans; //Here we declare a new dynamic array
+        vector<int> ans; 
         for(int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j] == target) { // now we equate the sum of array elements to the target value
-                    ans.push_back(i); // Here, we push the indices of elements to the araay
+                if(nums[i]+nums[j] == target) { 
+                    ans.push_back(i); 
                     ans.push_back(j);
                     break;
                 }
             }
         }
-        return ans; //we are returning the resultant array
+        return ans; 
     }
 };
