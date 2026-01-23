0441555b62fab1c13f87c2e84a403bb3ed0733db
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-01-23 23:39:16 +0530

    Time: 2 ms (43.02%) | Memory: 11.5 MB (17.97%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,16 +4,10 @@ public:
         stringstream ss(s);
         vector<string> words;
         string word;
-
-        // Extract words (automatically ignores extra spaces)
         while (ss >> word) {
             words.push_back(word);
         }
-
-        // Reverse words
-        reverse(words.begin(), words.end());
-
-        // Build result
+       reverse(words.begin(), words.end());
         string result;
         for (int i = 0; i < words.size(); i++) {
             result += words[i];
