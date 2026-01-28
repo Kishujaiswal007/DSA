5e3e732c7f32acfbacba82be3dd3f64b02959712
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-01-28 22:50:37 +0530

    Time: 9 ms (35.59%) | Memory: 11.3 MB (96.63%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool isFreqSame(vector<int> f1, vector<int> f2){
-        for(int i=0;i<26;i++){
-            if (f1[i] != f2[i]){
-                return false;
-            }
-        }return true;
-    }
     vector<int> findAnagrams(string s, string p) {
-        vector <int> f1(26,0);
-        for(int i=0;i<p.size();i++){
-            f1[p[i]-'a']++;
-        } 
-        int windowsize=p.size();
         vector<int> ans;
-        for(int i=0;i<s.size();i++){
-            int winindx=0, indx=i;
-            vector<int> f2(26,0);
-            while(winindx<windowsize && indx < s.size()){
-            f2[s[indx]-'a']++;
-            winindx++;
-            indx++;
-            
-            }
-         
-        if (winindx == windowsize && isFreqSame(f1, f2)) {
-            ans.push_back(i);
+        if (p.size() > s.size()) return ans;
+
+        vector<int> f1(26, 0), f2(26, 0);
+
+        for (char c : p)
+            f1[c - 'a']++;
+
+        int windowSize = p.size();
+
+        // first window
+        for (int i = 0; i < windowSize; i++)
+            f2[s[i] - 'a']++;
+
+        if (f1 == f2)
+            ans.push_back(0);
+
+        // slide window
+        for (int i = windowSize; i < s.size(); i++) {
+            f2[s[i] - 'a']++;                  // add right
+            f2[s[i - windowSize] - 'a']--;     // remove left
+
+            if (f1 == f2)
+                ans.push_back(i - windowSize + 1);
         }
 
-    } return ans;
+        return ans;
     }
 };
