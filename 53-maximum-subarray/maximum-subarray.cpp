9319a7e98ebf52cba829740ab6d43011581e6f1c
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-02-23 01:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.77%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int currSum=nums[0];
         int maxSum=nums[0];
         for(int i=1;i<nums.size();i++){
-            currSum=max(nums[i],currSum+nums[i]);
+            currSum=max(nums[i],currSum+nums[i]); // this will take care that if currsum is negative then we will start from that elem
             maxSum=max(maxSum,currSum);
         } return maxSum;
     }
