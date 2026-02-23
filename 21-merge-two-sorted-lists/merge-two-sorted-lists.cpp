9cecea270b0ea99204e644ed40bdb196d271188c
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-02-24 00:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (26.74%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -6,13 +6,12 @@ public:
         if(l1 == NULL) return l2;
         if(l2 == NULL) return l1;
 
-        // Recursive case
         if(l1->val < l2->val){
-            l1->next = mergeTwoLists(l1->next, l2);
+            l1->next=mergeTwoLists(l1->next,l2);
             return l1;
         }
         else{
-            l2->next = mergeTwoLists(l1, l2->next);
+             l2->next=mergeTwoLists(l2->next,l1);
             return l2;
         }
     }
