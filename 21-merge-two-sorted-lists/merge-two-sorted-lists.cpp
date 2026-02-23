1148ea78ad358af907e3806ee9fe6c7d0b4a01b1
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-02-23 23:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (86.23%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,37 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy=new ListNode(111);
-        ListNode* tail=dummy;
-        while(list1 != NULL && list2 != NULL){
-            if(list1->val <= list2->val){
-                tail->next=list1;
-                list1=list1->next;
-            }
-            else{
-                tail->next=list2;
-                list2=list2->next;
-            }
-            tail=tail->next;
-        }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
 
-        if(list1 != NULL){
-            tail->next=list1;
+        // Base cases
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+
+        // Recursive case
+        if(l1->val < l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
         }
         else{
-            tail->next=list2;
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
-
-        return dummy->next;
     }
 };
