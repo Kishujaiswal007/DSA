7acb0680d2f8ac4101f9e858ff170ecaa436a283
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-03-06 23:13:10 +0530

    Time: 1 ms (19.05%) | Memory: 9.4 MB (40.67%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    
-    void helper(string digits, int index, string current,
-                vector<string>& result,
-                unordered_map<char, string>& mp) {
-        
-        // Base case
-        if(index == digits.size()) {
+    void helper(int index, string digits,string current,vector<string> & result,unordered_map<char,string> &mp){
+        if(index==digits.size()){ 
             result.push_back(current);
             return;
         }
-        
-        // Get letters for current digit
         string letters = mp[digits[index]];
-        
-        for(char ch : letters) {
-            helper(digits, index + 1, current + ch, result, mp);
+        for(char ch: letters){
+            helper(index+1,digits,current+ch,result,mp);
         }
+
     }
-    
     vector<string> letterCombinations(string digits) {
-        
         if(digits.empty())
             return {};
-        
-        unordered_map<char, string> mp = {
-            {'2',"abc"}, {'3',"def"}, {'4',"ghi"},
-            {'5',"jkl"}, {'6',"mno"}, {'7',"pqrs"},
-            {'8',"tuv"}, {'9',"wxyz"}
-        };
-        
+        unordered_map<char,string> mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
         vector<string> result;
-        
-        helper(digits, 0, "", result, mp);
-        
+        helper(0,digits,"",result,mp);
         return result;
     }
 };
