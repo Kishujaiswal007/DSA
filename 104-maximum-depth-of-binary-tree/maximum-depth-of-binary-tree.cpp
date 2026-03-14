dcf4b4ba9d87304aba81573e17cc41ff9e23596d
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-03-14 18:08:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (47.77%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,7 +15,6 @@ public:
         if(root==NULL) return 0;
         int left=maxDepth(root->left);
         int right=maxDepth(root->right);
-        
-        return(max(left,right)+1);
+        return max(left,right)+1;
     }
 };
