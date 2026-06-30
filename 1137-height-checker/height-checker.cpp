0d21ecd42e5587a20806265d7e5dca49b02ff928
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-07-01 01:43:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (33.21%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> sortedHeights = heights;
-        sort(sortedHeights.begin(), sortedHeights.end());
-        int count = 0;
-        for (int i = 0; i < heights.size(); i++) {
-            if (heights[i] != sortedHeights[i]) {
-                count++;
-            }
+        vector<int> sorted=heights;
+        sort(sorted.begin(),sorted.end());
+        int count=0;
+        for(int i=0;i<heights.size();i++){
+            if(sorted[i]!=heights[i]) count++;
         }
         return count;
     }
