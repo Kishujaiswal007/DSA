09c695b9bbf3bbded10d85f6f74c0f5ca308a703
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-07-01 01:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (37.44%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -5,9 +5,9 @@ public:
         for(int i=0;i<nums.size();i++){
             int digits=0;
             while(nums[i]>0)
-            {
+            {digits++;
             nums[i]=nums[i]/10;
-            digits++;
+            
             }
             if(digits%2==0) count++;
         }
