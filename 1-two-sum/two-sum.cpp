2a2e74024b9db28b493e88b45a019a7619ef9ded
Kishu Jaiswal <84036149+Kishujaiswal007@users.noreply.github.com>
2026-09-19 00:30:39 +0530

    Time: 87 ms (7.46%) | Memory: 14.1 MB (76.26%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int need = target - nums[i];
-
-            if (mp.count(need)) {
-                return { mp[need], i };
+        vector<int> ans; //Here we declare a new dynamic array
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j] == target) { // now we equate the sum of array elements to the target value
+                    ans.push_back(i); // Here, we push the indices of elements to the araay
+                    ans.push_back(j);
+                    break;
+                }
             }
-
-            mp[nums[i]] = i;
         }
-        return {};
+        return ans; //we are returning the resultant array
     }
 };
